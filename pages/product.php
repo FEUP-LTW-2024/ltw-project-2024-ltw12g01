e90747b54628f0cf790e1c0c5b0cdf2dbeb901dd
Cruddle <up202107694@up.pt>
2024-04-28 17:34:50 +0100

    Fixed drawHeader

--- pages/product.php
+++ pages/product.php
@@ -2,11 +2,17 @@
 require_once('../templates/common.tpl.php');
 require_once(__DIR__ . '/../database/connection.db.php');
 require_once('../templates/items.tpl.php');
+
+require_once(__DIR__ . '/../session/session.php');
+$session = new Session();
+
 $db = getDatabaseConnection();
 
 $item = Item::getItem($db, intval($_GET['id']));
+
+drawHeader($session, true); 
+drawItem($item); 
+drawFooter(); 
+
 ?>
-<?php drawHeader(true); ?>
-<?php drawItem($item); ?>
-<?php drawFooter(); ?>
 
